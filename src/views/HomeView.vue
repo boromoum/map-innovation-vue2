a01d381eadae92872f1ff415fbf95e57359dd7fd
Borom OUM <borom.oum@chipmong.com>
2023-03-09 10:11:42 +0700

    Add tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-red-500">Home page</h1>
 </template>
